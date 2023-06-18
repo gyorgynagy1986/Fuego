db2e78b4ed7fa268ee8a3920fb8d19866677791d
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-06-18 11:27:36 +0200

    new update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
     <footer className={style.container}>
         <div className={style.containerTextleft}>
           <p className={style.containerTextleftCookieMobile} >Cookie policy</p>    
-            <Button url={'/'} underline={true} name={'GRUPO10'}/>
+            <Button url={'/'} underline={true} name={'GRUPO 10'}/>
             <div className={style.containerTextP}>
                 <p className={style.containerTextleftP}>Quisque ornare convallis quam sit amet hendrerit. Duis tincidunt condimentum eros ut laoreet. </p>
             </div>
              <Link href='/'>Learn more</Link>
             <div className={style.containerCookie}>
-                <div className={style.containerTextleftReserved}> <p> All rights reserved <span>©</span></p><p>Grupo10.hu</p> <p className={style.mobileTurn}>|</p> <p>2023</p> </div>
+                <div className={style.containerTextleftReserved}> <p> All rights reserved <span>©</span></p><p><a href="https://www.grupo10.hu/">Grupo10.hu</a></p> <p className={style.mobileTurn}>|</p> <p>2023</p> </div>
                 <p className={style.containerTextleftCookie} >Cookie policy</p>    
             </div>
         </div>
