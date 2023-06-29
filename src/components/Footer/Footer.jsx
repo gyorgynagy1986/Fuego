b8b7e1b4a4bdc2792757decba4d39b4c088da14c
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-06-29 12:16:09 +0200

    new update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,14 @@ import style from './Fotter.module.css'
 import Link from 'next/link'
 import Button from '../UI/Buttons/ButtonGetInTouch'
 
+const bromoStyle = {
+  marginTop: '1px',
+  textDecorationLine: 'unset',
+  color: '#FFF4E5',
+  fontSize: '12px'
+}
+
+const creatorlink = 'https://studiobromo.hu/'
 
 const Footer = () => {
   return (
@@ -13,9 +21,9 @@ const Footer = () => {
             <div className={style.containerTextP}>
                 <p className={style.containerTextleftP}></p>
             </div>
-             <Link href='https://www.grupo10.hu/'>Learn more</Link>
+            <Link href='https://www.grupo10.hu/'>Learn more about grupo 10</Link>
             <div className={style.containerCookie}>
-                <div className={style.containerTextleftReserved}> <p> All rights reserved <span>©</span></p><p>Grupo10.hu</p> <p className={style.mobileTurn}>|</p> <p>2023</p> </div>
+                <div className={style.containerTextleftReserved}> <p> All rights reserved <span>©</span></p><p>Grupo10.hu</p> <p className={style.mobileTurn}>|</p> <p>2023</p> <Link style={bromoStyle} href={creatorlink}>Website by Studio Bromo</Link> </div>
                 <p className={style.containerTextleftCookie} >Cookie policy</p>    
             </div>
         </div>
